dba1c35583904390ec7eb1ca16205638e6203321
francisco.lizana1801 <francisco.lizana1801@alumnos.ubiobio.cl>
2022-12-20 13:00:03 -0300

    Update

--- src/main/java/com/example/demo/controller/PerfilController.java
+++ src/main/java/com/example/demo/controller/PerfilController.java
@@ -19,25 +19,25 @@ public class PerfilController {
     private UsuarioServiceImplementation usuarioService;
 
     @GetMapping("")
-    public String perfil(  
-        @CookieValue(value = "id", defaultValue="0") String id_cookie,
-        @CookieValue(value = "rol", defaultValue="0") String rol,
-        Model model) {
+    public String perfil(
+            @CookieValue(value = "id", defaultValue = "0") String id_cookie,
+            @CookieValue(value = "rol", defaultValue = "0") String rol,
+            Model model) {
 
-            Usuario usuario = new Usuario();
-            usuario = usuarioService.getById(Integer.parseInt(id_cookie));
-            model.addAttribute("id", id_cookie);
-            model.addAttribute("rol", rol);
-            model.addAttribute("usuario", usuario);
+        Usuario usuario = new Usuario();
+        usuario = usuarioService.getById(Integer.parseInt(id_cookie));
+        model.addAttribute("id", id_cookie);
+        model.addAttribute("rol", rol);
+        model.addAttribute("usuario", usuario);
         return "perfil/perfil";
     }
-    
+
     @PostMapping("/save")
-    public String save( 
-        @CookieValue(value = "id", defaultValue="0") String id_cookie,
-        @CookieValue(value = "rol", defaultValue="0") String rol,
-        Model model,
-        @Valid Usuario user) {
+    public String save(
+            @CookieValue(value = "id", defaultValue = "0") String id_cookie,
+            @CookieValue(value = "rol", defaultValue = "0") String rol,
+            Model model,
+            @Valid Usuario user) {
 
         usuarioService.saveUsuario(user);
         Usuario usuario = new Usuario();
