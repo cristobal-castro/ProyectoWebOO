439160c01a11210d5cd914070da1f0cab377d52b
francisco.lizana1801 <francisco.lizana1801@alumnos.ubiobio.cl>
2022-12-20 12:59:12 -0300

    Update

--- src/main/java/com/example/demo/controller/PerfilController.java
+++ src/main/java/com/example/demo/controller/PerfilController.java
@@ -1,20 +1,13 @@
 package com.example.demo.controller;
 
-import java.io.Console;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.example.demo.model.Usuario;
-import com.example.demo.services.PerfilServiceImplementation;
 import com.example.demo.services.UsuarioServiceImplementation;
 
 import jakarta.validation.Valid;
@@ -23,22 +16,35 @@ import jakarta.validation.Valid;
 @RequestMapping("/perfil")
 public class PerfilController {
     @Autowired
-    private UsuarioServiceImplementation usuarioServiceImplementation;
+    private UsuarioServiceImplementation usuarioService;
 
     @GetMapping("")
-    public String perfil( @CookieValue(value = "id", defaultValue = "-1") int idUser, Model model) {
-        System.out.println("COOKIE ID:" + idUser);
-        if(idUser<0) return "error";
-        Usuario user = usuarioServiceImplementation.getById(idUser);
-        model.addAttribute("usuario", user);
+    public String perfil(  
+        @CookieValue(value = "id", defaultValue="0") String id_cookie,
+        @CookieValue(value = "rol", defaultValue="0") String rol,
+        Model model) {
+
+            Usuario usuario = new Usuario();
+            usuario = usuarioService.getById(Integer.parseInt(id_cookie));
+            model.addAttribute("id", id_cookie);
+            model.addAttribute("rol", rol);
+            model.addAttribute("usuario", usuario);
         return "perfil/perfil";
     }
     
     @PostMapping("/save")
-    public String save( Model model,@Valid Usuario user) {
-        usuarioServiceImplementation.saveUsuario(user);
-        System.out.println("ID:" + user.getId());
-        model.addAttribute("usuario", user);
+    public String save( 
+        @CookieValue(value = "id", defaultValue="0") String id_cookie,
+        @CookieValue(value = "rol", defaultValue="0") String rol,
+        Model model,
+        @Valid Usuario user) {
+
+        usuarioService.saveUsuario(user);
+        Usuario usuario = new Usuario();
+        usuario = usuarioService.getById(Integer.parseInt(id_cookie));
+        model.addAttribute("id", id_cookie);
+        model.addAttribute("rol", rol);
+        model.addAttribute("usuario", usuario);
         return "redirect:/perfil";
     }
 
